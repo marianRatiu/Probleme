bf4d12638ec80cad948549655ea60cded9d6aa30
marianRatiu <marian.ratiu@3pillarglobal.com>
2024-07-12 10:34:06 +0300

    Update #21.py

--- #21.py
+++ #21.py
@@ -2,15 +2,10 @@
 
 def mygetter(*args):
     if len(args) == 1:
-
-        def single_item_getter(item):
-            return item[args[0]]
-        return single_item_getter
+        return lambda item: item[args[0]]
     else:
-        def multiple_item_getter(item):
-            return tuple(item[arg]for arg in args)
-        return multiple_item_getter
-
+        return lambda item: tuple(item[arg] for arg in args)
+        
 
 
 g1 = mygetter(0,-1)
